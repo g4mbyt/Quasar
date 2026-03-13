8e0c4d7d447b6f8983210e67bc87c9bda832187f
Gergő-Vince Bús <bus.gergo@gmail.com>
2026-03-13 18:00:25 +0200

    ios and docs init

---
+++ quasar-ios/quasar-iosTests/quasar_iosTests.swift
@@ -0,0 +1,17 @@
+//
+//  quasar_iosTests.swift
+//  quasar-iosTests
+//
+//  Created by Gergő-Vince Bús on 13/03/2026.
+//
+
+import Testing
+@testable import quasar_ios
+
+struct quasar_iosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
