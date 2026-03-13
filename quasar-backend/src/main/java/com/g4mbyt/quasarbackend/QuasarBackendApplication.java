ce07fc094a8335239c6e3a8d71327f0748ede835
Gergő-Vince Bús <bus.gergo@gmail.com>
2026-03-13 17:53:19 +0200

    init

---
+++ quasar-backend/src/main/java/com/g4mbyt/quasarbackend/QuasarBackendApplication.java
@@ -0,0 +1,13 @@
+package com.g4mbyt.quasarbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuasarBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuasarBackendApplication.class, args);
+    }
+
+}
---
+++ quasar-backend/src/test/java/com/g4mbyt/quasarbackend/QuasarBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.g4mbyt.quasarbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuasarBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
